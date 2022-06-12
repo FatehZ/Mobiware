7cdd9159913f4fa47bcf20ebafa93ca14fb2f598
fateh <ktx.development@gmail.com>
2022-06-12 14:38:08 +0400

    Finishing recyclerview

--- app/src/main/java/com/ktxdevelopment/mobiware/ui/activities/SignInActivity.kt
+++ app/src/main/java/com/ktxdevelopment/mobiware/ui/activities/SignInActivity.kt
@@ -1,86 +1,106 @@
 package com.ktxdevelopment.mobiware.ui.activities
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Toast
-import androidx.core.os.postDelayed
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.FirebaseAuth
 import com.ktxdevelopment.mobiware.clients.BaseClient
 import com.ktxdevelopment.mobiware.clients.BaseClient.hasInternetConnection
 import com.ktxdevelopment.mobiware.clients.BaseClient.whichModelSuits
+import com.ktxdevelopment.mobiware.clients.FirestoreClient
 import com.ktxdevelopment.mobiware.clients.TextInputClient.validateSignInInput
+import com.ktxdevelopment.mobiware.clients.ui.SignInClient.initializeRecyclerView
 import com.ktxdevelopment.mobiware.databinding.ActivitySignInBinding
-import com.ktxdevelopment.mobiware.models.rest.product.Data
 import com.ktxdevelopment.mobiware.models.rest.search.Phone
-import com.ktxdevelopment.mobiware.util.Constants
-import com.ktxdevelopment.mobiware.viewmodel.RetrofitViewModel
+import com.ktxdevelopment.mobiware.ui.recview.SelectionAdapter
+import com.ktxdevelopment.mobiware.ui.recview.SelectionAdapter.OnMobileClickListener
+import com.ktxdevelopment.mobiware.viewmodel.BaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class SignInActivity : BaseActivity() {
+class SignInActivity : BaseActivity(), OnMobileClickListener {
     private lateinit var binding: ActivitySignInBinding
-    private lateinit var restViewModel: RetrofitViewModel
+    private lateinit var restViewModel: BaseViewModel
     private val TAG = "SIGN_IN_TAG"
     private var isResponsePresent: MutableLiveData<Boolean> = MutableLiveData()
+    private lateinit var mobileAdapter: SelectionAdapter
 
-    private lateinit var phone: Phone
-    private lateinit var phoneDetails: Data
+    private lateinit var phones: List<Phone>
+    private var selectedPhoneUrl: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        startActivity(Intent(this, MainActivity::class.java))
-
-        restViewModel = ViewModelProvider(this)[RetrofitViewModel::class.java]
+        restViewModel = ViewModelProvider(this)[BaseViewModel::class.java]
         restViewModel.searchMobile(BaseClient.getDeviceModel())
-
         binding.btnSignIn.setOnClickListener { signButtonClickListener() }
+
+
 
         restViewModel.searchResponse.observe(this) { response ->
             if (response.isSuccessful) if (response.body() != null) if (response.body()!!.data.phones.isNotEmpty()) {
-                phone = whichModelSuits(response.body()!!.data.phones)
-                restViewModel.getMobile(phone.detail)
-
-                restViewModel.getResponse.observe(this@SignInActivity) {
-                    if (it.isSuccessful) if (it.body()!= null) {
-                        isResponsePresent.value = true
-                        phoneDetails = it.body()!!.data
-                    }
-                }
+                phones = whichModelSuits(response.body()!!.data.phones)
+                mobileAdapter = SelectionAdapter(this)
+                initializeRecyclerView(this, binding, phones, mobileAdapter)
+                mobileAdapter.setData(phones)
+                binding.tvSignUseless
             }
         }
     }
 
+
     private fun signButtonClickListener() {
-        if (validateSignInInput(binding.etUsernameSignIn, binding.etUsernameSignIn.text.toString())) {
+        if (validateSignInInput(binding.etUsernameSignIn, binding.etPasswordSignIn ,binding.etUsernameSignIn.text.toString())) {
             if (hasInternetConnection(this)) {
-                val mainIntent = Intent(this@SignInActivity, MainActivity::class.java).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK) }
+
+                FirestoreClient.registerTest()
 
 
-
-                if (isResponsePresent.value == true) {
-                    BaseClient.handler.postDelayed(1000) { startActivity(mainIntent) }
-
-                }else{
-                    binding.clProgressSignIn.visibility = VISIBLE
-                    isResponsePresent.observe(this@SignInActivity) {
-                        if (it) {
-                            mainIntent.putExtra(Constants.PHONE_EXTRA, phone)
-                            mainIntent.putExtra(Constants.PHONE_DETAILS_EXTRA, phoneDetails)
-                            //Write to internal db
-//                        Preferences.storeIsFirstRun(false)
-                        }
-                    }
-                }
-            }else{
+            } else {
                 Toast.makeText(this, "Please check internet connection", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+
+    override fun onMobileClick(position: Int) {
+        selectedPhoneUrl = phones[position].detail
+        if (binding.tvSignPhoneModel.visibility == GONE) binding.tvSignPhoneModel.visibility = VISIBLE
+        binding.tvSignPhoneModel.text = phones[position].phone_name
+    }
 }
+
+
+
+
+
+
+
+
+
+
+//                val mainIntent = Intent(this@SignInActivity, MainActivity::class.java).apply {
+//                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK)
+//                }
+
+
+//                if (isResponsePresent.value == true) {
+//                    BaseClient.handler.postDelayed(1000) { startActivity(mainIntent) }
+//
+//                } else {
+//                    isResponsePresent.observe(this@SignInActivity) {
+//                        if (it) {
+//                            mainIntent.putExtra(Constants.PHONE_EXTRA, phones[0])
+//                            mainIntent.putExtra(Constants.PHONE_DETAILS_EXTRA, phoneDetails)
+//                            //Write to internal db
+////                        Preferences.storeIsFirstRun(false)
+//                        }
+//                    }
+//                }
